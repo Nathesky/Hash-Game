075c71d14a701e9b26c47786a28ee1d8a8c9d6bd
Nathesky <132522541+Nathesky@users.noreply.github.com>
2023-05-08 20:35:27 -0300

    Add files via upload

---
+++ hash.game/javascript/script.js
@@ -0,0 +1,44 @@
+function validarInput(event) {
+    const tecla = event.key;
+    if (tecla !== "X" && tecla !== "O" && tecla !== "x" && tecla !== "o") {
+    event.preventDefault();
+      alert("Por favor, digite apenas X ou O");
+      event.target.value = "";
+    }
+  }
+  
+function res() {
+    
+    var matriz = [];
+    for (var i = 1; i <= 9; i++) {
+        var letras = document.getElementById("i" + i).value.toLowerCase();
+        matriz.push(letras);
+    }
+
+    var vitorias = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+
+    for (var i = 0; i < vitorias.length; i++) {
+        var [a, b, c] = vitorias[i];
+        if (matriz[a] && matriz[a] === matriz[b] && matriz[a] === matriz[c]) {
+            setTimeout(function () {
+                alert(matriz[a].toUpperCase() + " ganhou");
+            }, 200);
+            return;
+        }
+    }
+
+    for (var i = 0; i < matriz.length; i++) {
+        if (!matriz[i]) {
+            return;
+        }
+    }
+    setTimeout(function () {
+        alert("Empate");
+    }, 100);
+
+}
